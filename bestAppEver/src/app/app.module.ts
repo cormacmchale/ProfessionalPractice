7529d60f3e76870224d3d3fa71877c299e2c2d89
makarenkoelena <g00341964@gmit.ie>
2019-01-31 15:51:28 +0000

    google maps plugin added

--- bestAppEver/src/app/app.module.ts
+++ bestAppEver/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapsComponent } from './maps/maps.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MapsComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
